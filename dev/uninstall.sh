e4f1b54ab1a73fd6fd6de19bf122d19f77dcda20
Owen <owen.debiasio@gmail.com>
2026-07-27 16:15:11 -0400

    Add proper dev install/uninstall scripts

---
+++ dev/uninstall.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo removing oiff...
+sudo rm /usr/local/bin/oiff
+
+echo removing oiff viewer...
+sudo rm /usr/share/applications/oiff-viewer.desktop /usr/share/icons/hicolor/scalable/apps/oiff-viewer.svg
+
+echo removing oiff thumbnailer...
+sudo rm /usr/share/thumbnailers/oiff.thumbnailer /usr/share/mime/packages/oiff.xml
+
+echo refreshing thumbnails...
+
+rm -rf ~/.cache/thumbnails/*
+sudo update-mime-database /usr/share/mime
+
+echo Uninstalled!
